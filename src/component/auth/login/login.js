5ab8e2f2110aa4a43c646760750256d5b52718c4
Akram3498 <akram.hanif@liqteq.com>
2022-02-08 18:13:52 +0500

    FB Authorization

--- src/component/auth/login/login.js
+++ src/component/auth/login/login.js
@@ -11,7 +11,9 @@ import {
 import { useHistory } from "react-router-dom";
 import { Login as LoginUser } from '../../../services/apiInteraction';
 
-
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
+  
 
 // import CSS 
 import '../auth.css'
@@ -29,12 +31,56 @@ const validateMessages = (data) => {
     notification.error(args);
 };
 
+const oauth = {
+    domain: 'wo.auth.us-east-2.amazoncognito.com',
+    scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+    redirectSignIn: 'https://app.whoson.co/',
+    redirectSignOut: 'https://app.whoson.co/',
+    responseType: 'token'
+};
+
+Auth.configure({
+    region: "us-east-2",
+    userPoolId: "us-east-2_aPujjAawB",
+    userPoolWebClientId: "6u3bu80bhobl8rts163gc022m",      
+    oauth 
+});
+
+Amplify.configure({
+    aws_cognito_region: "us-east-2", // (required) - Region where Amazon Cognito project was created   
+    aws_user_pools_id:  "us-east-2_aPujjAawB", // (optional) -  Amazon Cognito User Pool ID   
+    aws_user_pools_web_client_id: "6u3bu80bhobl8rts163gc022m", // (optional) - Amazon Cognito App Client ID (App client secret needs to be disabled)
+    aws_cognito_identity_pool_id: "us-east-2_aPujjAawB", // (optional) - Amazon Cognito Identity Pool ID   
+    aws_mandatory_sign_in: "enable" // (optional) - Users are not allowed to get the aws credentials unless they are signed in   
+  })
 
 function Login() {
+    const [user, setUser] = useState(null);
+    const [customState, setCustomState] = useState(null);
 
+    
     useEffect(() => {
-    }, [])
-
+        const unsubscribe = Hub.listen("auth", ({ payload: { event, data } }) => {
+          switch (event) {
+            case "signIn":
+              setUser(data);
+              break;
+            case "signOut":
+              setUser(null);
+              break;
+            case "customOAuthState":
+              setCustomState(data);
+          }
+        });
+    
+        Auth.currentAuthenticatedUser()
+          .then(currentUser => setUser(currentUser))
+          .catch((err) => console.log(err));
+    
+        return unsubscribe;
+      }, []);
+    
+    
 
     const [loader, setLoader] = useState(false)
 
@@ -93,7 +139,15 @@ function Login() {
     }
 
     return (
+  
         <div style={{ height: '100vh', position: 'relative' }} className="gray-background">
+        <div className="App">
+        <button onClick={() => Auth.federatedSignIn({provider: CognitoHostedUIIdentityProvider.Facebook })}>Open Facebook</button>
+        <button onClick={() => Auth.federatedSignIn({provider: CognitoHostedUIIdentityProvider.Google })}>Open Google</button>
+        <button onClick={() => Auth.federatedSignIn()}>Open Hosted UI</button>
+        {/* <button onClick={() => Auth.signOut()}>Sign Out {user?.getUsername()}</button> */}
+      </div>
+
             <Spin className="loader" spinning={loader} size="large" />
             <Row style={{ height: '100vh', position: 'relative' }}>
                 <Col md={8} xs={24} >
