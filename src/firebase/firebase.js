2a899e9222cce9ab4505151fa551d7ce81729b9d
umairrehman0305 <umair.rehman@liqteq.com>
2022-03-15 17:56:26 +0500

    firbase config change

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,7 +4,7 @@ import firebase from 'firebase';
 const config = {
   apiKey: "AIzaSyA49mVYSNLXZQqWfq-sJi-MRofZI06u8U8",
   authDomain: "who-s-on.firebaseapp.com",
-  projectId: "who-s-on",
+  projectId: "whos-on-340806",
   storageBucket: "who-s-on.appspot.com",
   messagingSenderId: "1062275279679",
   appId: "1:1062275279679:web:d3402b9b1c819df656c18a",
@@ -15,7 +15,6 @@ firebase.initializeApp(config);
 
 
 export default firebase
-
 
 
 
