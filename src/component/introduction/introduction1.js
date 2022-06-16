22a78649ac0907934372240270ee15691350844a
Hassan Khan <hassan.ahmed@liqteq.com>
2022-06-16 12:01:55 +0500

    TEST | mobile platform

--- src/component/introduction/introduction1.js
+++ src/component/introduction/introduction1.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Row, Col, Image, Typography, Button } from 'antd';
 import introductionImage from '../../assets/images/introduction1.png'
 import {
@@ -7,10 +7,34 @@ import {
 
 import './introduction.css'
 
-const { Text, Title  } = Typography;
+const { Text, Title } = Typography;
 
 
 function Introduction1() {
+
+    const test = () => {
+        if (
+            navigator.userAgent.match(/Android/i) ||
+            navigator.userAgent.match(/iPhone/i)
+        ) {
+            alert("mobile")
+        }
+        else{
+            alert("web")
+
+        }
+    }
+
+
+    useEffect(() => {
+        test()
+
+    }, [])
+
+
+
+
+
     return (
         <Row >
             <Col className="full-height mobile-responsive" span={12} >
@@ -22,15 +46,15 @@ function Introduction1() {
                     src={introductionImage}
                 />
             </Col>
-            <Col md={12} xs={24} className="padding-auto" style={{height:'100vh'}} span={12}>
+            <Col md={12} xs={24} className="padding-auto" style={{ height: '100vh' }} span={12}>
                 <Row>
                     <Link to='/login'>
                         <p className="justify-content-right d-flex w-100 g-color">Sign In</p>
                     </Link>
                 </Row>
-                
+
                 <Row className="mt-5">
-                    <Title className="intro-heading" >Welcome <br/>to Who's On!</Title>
+                    <Title className="intro-heading" >Welcome <br />to Who's On!</Title>
                 </Row>
 
                 <Row>
@@ -39,7 +63,7 @@ function Introduction1() {
 
                 <Row className="d-flex fixed-button ">
                     <Link to="/instruction-2">
-                        <Button className="button mt-5"  type="primary">Next</Button>
+                        <Button className="button mt-5" type="primary">Next</Button>
                     </Link>
                 </Row>
 
